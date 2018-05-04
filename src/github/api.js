d595519ad6e04d77d2baaf4d185668ac66cf9b74
Mikael Lisselgård <mikael.lisselgard@standout.se>
2018-05-04 13:44:36 +0200

    Normalize response

--- src/github/api.js
+++ src/github/api.js
@@ -9,6 +9,26 @@ const client = new GraphQLClient('https://api.github.com/graphql', {
 
 exports.getRequestedReviewers = function(owner) {
   return client.request(queries.getRequestedReviewers(owner))
-    .then(data => data)
+    .then((data) => {
+      const result = { pullRequests: [] }
+      data.organization.repositories.edges.forEach((repo) => {
+        repo.node.pullRequests.edges.forEach((pr) => {
+          const resp = {
+            repository: repo.node.name,
+            user: pr.node.author.login,
+            title: pr.node.title,
+            additions: pr.node.additions,
+            deletions: pr.node.deletions,
+            created_at: pr.node.createdAt,
+            url: `https://github.com${pr.node.resourcePath}`,
+            reviewers: pr.node.reviewRequests.edges.map((reviewRequest) => {
+              return reviewRequest.node.requestedReviewer.login
+            })
+          }
+          result.pullRequests.push(resp)
+        })
+      })
+      return result
+    })
     .catch(err => err)
 }
